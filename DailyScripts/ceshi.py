1e1ccaf537770fd1762828bea84c8b75e4b56330
zhanggonghao <18217679359@163.com>
2026-05-25 16:15:44 +0800

    覆盖更新，删除远程多余目录

--- DailyScripts/ceshi.py
+++
@@ -1,9 +0,0 @@
-
-from ultis.stock_data_client import StockDataClient
-client = StockDataClient(data_path='/home/samba/Market')
-
-pre_date = '2026-04-21'
-sample_range_df = client.get_stock_index_comments('zz1800', start=pre_date, end=pre_date).T
-sample_range_df = sample_range_df[sample_range_df[pre_date] == 1]
-print(sample_range_df)
-print(sample_range_df.index.tolist())
--- DailyScripts/link_ubuntu.py
+++
@@ -1,7 +0,0 @@
-
-from ultis.stock_data_client import StockDataClient
-client = StockDataClient(data_path=r'Z:\Market')
-
-data = client.get_stock_index_comments_weights('000300.XSHG', start='2026-05-06', end='2026-05-06')
-
-print(data)
